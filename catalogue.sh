82d6814f4853f76eeaaec598a364841cd3f1b5e9
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-29 17:54:14 +0530

    mogodb

--- catalogue.sh
+++ catalogue.sh
@@ -73,7 +73,7 @@ systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
 VALIDATE $? "Enable catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copy mango repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
